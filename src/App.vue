b3a1d9f75304b53b7485a6093f845f46d0b19cc9
SabaMuladze <muladze66@bk.ru>
2023-12-06 23:36:55 +0400

    progress in TheHeader

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 
 <template>
-  
+  <the-header></the-header>
 </template>
 
 <script >
+import TheHeader from './components/TheHeader.vue';
+export default{
+  components:{
+    TheHeader
+  }
+}
 </script>
 
 <style scoped>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,52 @@
+<template >
+    <div>
+        <header>
+        <img src="../assets/images/logo.svg" alt="logo">
+        <img :src="outputBurgerPic" @click="changeBurgerPic">
+    </header>
+    <div>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+            <li>Blog</li>
+            <li>Carre</li>
+        </ul>
+    </div>
+    </div>
+    
+
+</template>
+
+<script>
+export default{
+data() {
+    return {
+        menu: false,
+        burger:'src/assets/images/icon-hamburger.svg',
+        closeBurger: 'src/assets/images/icon-close.svg'
+    }
+    
+},
+methods: {
+        changeBurgerPic(){
+            this.menu = !this.menu
+        }
+    },
+    computed:{
+        outputBurgerPic(){
+            return this.menu === false ? this.burger : this.closeBurger 
+        }
+    }
+}
+</script>
+
+<style scoped>
+header{
+    display: flex;
+    justify-content: space-between;
+    padding: 0 32px;
+    align-items: center;
+    min-height: 60px;
+}
+</style>
